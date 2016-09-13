e47ada0e2d7db0dde6635951b71f68ca2b1da39b
Serhii Kobzar <serhii_kobzar@epam.com>
2016-09-13 15:26:20 +0300

    Коммит фалов .java

---
+++ DayTwoJava/src/main/java/com/company/ArihtmeticOperation.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by Serhii_Kobzar on 9/13/2016.
+ */
+public class ArihtmeticOperation {
+    public double add (double value1, double value2){
+        return value1 + value2;
+    }
+
+    public double deduct(double value1, double value2){
+        return value1 - value2;
+    }
+
+    public double mult(double value1, double value2){
+        return value1 * value2;
+    }
+
+    public double div(double value1, double value2){
+        return  value1 / value2;
+    }
+}
---
+++ DayTwoJava/src/test/java/com/company/ArihtmeticOperationTest.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Serhii_Kobzar on 9/13/2016.
+ */
+public class ArihtmeticOperationTest {
+    @Test
+    public void deduct() throws Exception {
+        ArihtmeticOperation a = new ArihtmeticOperation();
+        double res = a.deduct(7,3);
+        if (res != 4) Assert.fail();
+    }
+
+    @Test
+    public void mult() throws Exception {
+        ArihtmeticOperation a = new ArihtmeticOperation();
+        double res = a.mult(3,7);
+        if (res != 21) Assert.fail();
+    }
+
+    @Test
+    public void div() throws Exception {
+        ArihtmeticOperation a = new ArihtmeticOperation();
+        double res = a.div(4,2);
+        if (res != 2) Assert.fail();
+    }
+
+    @Test
+    public void add() throws Exception {
+        ArihtmeticOperation a = new ArihtmeticOperation();
+        double res = a.add(3,7);
+        Assert.assertTrue(res == 10);
+    }
+
+}
