307a2dc215ec379cc8e75e7940f2bf28a9b09ba1
Serhii Kobzar <serhii_kobzar@epam.com>
2016-09-13 15:46:53 +0300

    using Before in tests

--- DayTwoJava/src/test/java/com/company/ArihtmeticOperationTest.java
+++ DayTwoJava/src/test/java/com/company/ArihtmeticOperationTest.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,32 +10,44 @@ import static org.junit.Assert.*;
  * Created by Serhii_Kobzar on 9/13/2016.
  */
 public class ArihtmeticOperationTest {
+
+    private ArihtmeticOperation a;
+    
+    @Before
+    public void runT(){
+        a = new ArihtmeticOperation();
+    }
+
     @Test
     public void deduct() throws Exception {
-        ArihtmeticOperation a = new ArihtmeticOperation();
+        runT();
         double res = a.deduct(7,3);
         Assert.assertEquals(4.0, res, 0);
+        a = null;
     }
 
     @Test
     public void mult() throws Exception {
-        ArihtmeticOperation a = new ArihtmeticOperation();
+        runT();
         double res = a.mult(3,7);
         Assert.assertEquals(21.0, res, 0);
+        a = null;
     }
 
     @Test
     public void div() throws Exception {
-        ArihtmeticOperation a = new ArihtmeticOperation();
+        runT();
         double res = a.div(4,2);
         Assert.assertEquals(2.0, res, 0);
+        a = null;
     }
 
     @Test
     public void add() throws Exception {
-        ArihtmeticOperation a = new ArihtmeticOperation();
+        runT();
         double res = a.add(3,7);
         Assert.assertEquals(10.0, res, 0);
+        a = null;
     }
 
 }
