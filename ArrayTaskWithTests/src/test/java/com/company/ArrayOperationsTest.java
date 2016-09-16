831a7ab451d787804a99ae1c3aa854fbc620da65
Serhii Kobzar <serhii_kobzar@epam.com>
2016-09-16 13:56:15 +0300

    Added searchingNumber into tests

--- ArrayTaskWithTests/src/test/java/com/company/ArrayOperationsTest.java
+++ ArrayTaskWithTests/src/test/java/com/company/ArrayOperationsTest.java
@@ -10,60 +10,57 @@ import static org.junit.Assert.*;
  */
 public class ArrayOperationsTest {
 
-
-    int notEmptyArray[] = {2,3,4,5,6,2,4,7};
     int emptyArray[] = {};
     int oneElementArray[]={1};
     int [] moreThenOneResult = {0,5,7};
-
-
+    int serchingValue = 1;
 
     @Test
     public void tesFindIndexesOfArrayEmpryArray() throws Exception {
         int[] emptyArrayForTestRes = {};
-        int[] res = ArrayOperations.findIndexesOfArray(emptyArray,3);
+        int[] res = ArrayOperations.findIndexesOfArray(emptyArray,serchingValue);
         Assert.assertArrayEquals(res,emptyArrayForTestRes);
     }
 
     @Test
     public void testFindIndexesOfArrayOneElementArrayWithElementIncluded() throws Exception {
         int[] oneElementArraForTest= {1};
-        int[] res = ArrayOperations.findIndexesOfArray(oneElementArraForTest,1);
+        int[] res = ArrayOperations.findIndexesOfArray(oneElementArraForTest,serchingValue);
         Assert.assertEquals(oneElementArray.length,res.length);
     }
 
     @Test
     public void testFindIndexesOfArrayOneElementArrayWithElementExcluded() throws Exception{
-        int[] oneElementArraForTest= {1};
-        int[] res = ArrayOperations.findIndexesOfArray(oneElementArraForTest,2);
+        int[] oneElementArraForTest= {2};
+        int[] res = ArrayOperations.findIndexesOfArray(oneElementArraForTest,serchingValue);
         Assert.assertEquals(emptyArray.length,res.length);
     }
 
     @Test
     public void testFindIndexesOfArrayMoreThenOneElement(){
         int[] moreThenOneTest = {1,2,3,4,5,1,3,1};
-        int[] res = ArrayOperations.findIndexesOfArray(moreThenOneTest,1);
+        int[] res = ArrayOperations.findIndexesOfArray(moreThenOneTest,serchingValue);
         Assert.assertArrayEquals(res,moreThenOneResult);
     }
 
     @Test
     public void findCountOfValueInEmptyArray(){
         int[] arrayFroEmtyCount = {};
-        int res = ArrayOperations.findCountOfValueInArray(arrayFroEmtyCount,1);
+        int res = ArrayOperations.findCountOfValueInArray(arrayFroEmtyCount,serchingValue);
         Assert.assertEquals(res,0);
     }
 
     @Test
     public void findCountOfValueInNotEmtyArrayWithoutSearchingElement(){
-        int arrayWithElements[] = {1,2,3,4,5,6,7};
-        int res = ArrayOperations.findCountOfValueInArray(arrayWithElements,22);
+        int arrayWithElements[] = {2,3,4,5,6,7};
+        int res = ArrayOperations.findCountOfValueInArray(arrayWithElements,serchingValue);
         Assert.assertEquals(res,0);
     }
 
     @Test
     public void findCountOfValueInNotEmtyArrayWithSearchingElement(){
         int arrayWithElements[] = {1,2,3,4,5,6,7,1,1};
-        int res = ArrayOperations.findCountOfValueInArray(arrayWithElements,1);
+        int res = ArrayOperations.findCountOfValueInArray(arrayWithElements,serchingValue);
         Assert.assertEquals(res,3);
     }
 }
